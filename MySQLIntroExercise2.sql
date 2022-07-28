21573c20da137c22574bbb1bc13e7580151f4c52
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-28 15:45:27 -0400

    Corrected price average and geek squad middle initial

--- MySQLIntroExercise2.sql
+++ MySQLIntroExercise2.sql
@@ -48,13 +48,13 @@ FROM Products
 WHERE OnSale = 1;
 
 -- find the average price of all products
-SELECT AVG(Price) AS "Average of Products"
+SELECT CONCAT("$", ROUND(AVG(Price), 2)) AS "Average of Products"
 FROM Products;
 
 -- find all Geek Squad employees who don't have a middle initial
 SELECT *
 FROM Employees
-WHERE Title LIKE "%Geek%Squad%" AND MiddleName IS NULL;
+WHERE Title LIKE "%Geek%Squad%" AND MiddleInitial IS NULL;
 
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
 SELECT *
