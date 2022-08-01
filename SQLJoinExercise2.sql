9035e62f81f7d6157d665505bf7ce95d51d9dd7d
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-08-01 10:39:36 -0400

    Found name of department and catgeory for appliances and games

--- SQLJoinExercise2.sql
+++ SQLJoinExercise2.sql
@@ -14,18 +14,25 @@ ON products.ProductID = reviews.ProductID
 WHERE reviews.Rating = 5;
 
 /* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
-SELECT employees.EmployeeID
+SELECT  employees.EmployeeID, employees.FirstName, employees.MiddleInitial, employees.LastName, Sum(sales.Quantity) AS Total
 FROM employees
 INNER JOIN sales
 ON employees.EmployeeID = sales.EmployeeID
-WHERE sales.Quantity
+GROUP BY employees.EmployeeID
+ORDER BY Total DESC
+LIMIT 2;
 
 /* joins: find the name of the department, and the 
 name of the category for Appliances and Games */
+SELECT departments.Name, categories.Name
+FROM departments
+INNER JOIN categories
+ON categories.DepartmentID = departments.DepartmentID
+WHERE categories.Name = "Appliances" OR categories.Name = "Games";
 
 /* joins: find the product name, total # sold, and total price sold,
  for Eagles: Hotel California --You may need to use SUM() */
-
+SELECT products.Name, 
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
 
 
